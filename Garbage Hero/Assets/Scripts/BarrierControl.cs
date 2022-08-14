af767ca771e2461292d6b7da259921487694dfc7
chicaaago <70597512+thomasoliver2021@users.noreply.github.com>
2022-08-14 19:07:19 -0400

    Trash now accumulates in the barrier in a spiral pattern

--- Garbage Hero/Assets/Scripts/BarrierControl.cs
+++ Garbage Hero/Assets/Scripts/BarrierControl.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class BarrierControl : MonoBehaviour
 {
     List<SpriteRenderer> trashInBarrier;
-    // Start is called before the first frame update
+    GameObject player;
+
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+
         trashInBarrier = new List<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Initial radius and theta for the spiral
+        float radius = 0.15f;
+        float theta = 0f;
+        foreach(var trash in trashInBarrier){
+            // Calculate the x and y positions for every piece of trash in the barrier
+            float xPos = player.transform.position.x + (radius * Mathf.Cos(theta));
+            float yPos = player.transform.position.y + (radius * Mathf.Sin(theta));
+
+            // Move trash to proper location
+            trash.transform.position = new Vector2(xPos, yPos);
+
+            // Increment the angle
+            theta += 0.6f;
+            // Increment the radius
+            radius += 0.01f;
+        }
+
     }
 
     public void addTrashToArray(SpriteRenderer newTrash){
+        // Add trash from space into the barrier
         trashInBarrier.Add(newTrash);
     }
 }
