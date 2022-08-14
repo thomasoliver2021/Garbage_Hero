6bf9d1ab0c658c09d84658a00d1f067b54111855
chicaaago <70597512+thomasoliver2021@users.noreply.github.com>
2022-08-14 01:41:32 -0400

    Added ship movement

--- Garbage Hero/Assets/Scripts/PlayerMovement.cs
+++ Garbage Hero/Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerMovement : MonoBehaviour
 {
+    // Change the speed of the ship
+    // MAKE PRIVATE LATER, public just for testing rn!
+    public float speed = 0.7f;
     void Update(){
+        // Get mouse position
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Calculate angle between ship and mouse
         var dir = mousePosition - transform.position;
         var angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) - 90;
+
+        // Calculate how much the ship should accelerate
+        // The further the mouse, the faster the ship will go
+        float acceleration = Vector2.Distance(mousePosition, transform.position) * speed;
+
+        // Rotate ship to face mouse
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        // Move the ship towards the mouse
+        transform.position = Vector2.MoveTowards(transform.position, mousePosition, acceleration * Time.deltaTime);
     }
 }
